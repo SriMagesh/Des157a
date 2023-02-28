014c92db59d36bd2876382f7c5342b6bac2ac8fa
SriramMagesh <SriramMagesh@srmagesh.com>
2023-02-28 02:08:17 -0800

    Every picture

---
+++ studio 2/script.js
@@ -0,0 +1,17 @@
+
+(function(){
+
+    const scrollContainer = document.querySelector("main");
+
+    const frame = document.querySelector('.image-frame');
+    const image = frame.querySelector('.image1');
+
+    // Set the width and height of the frame to match the image size
+    frame.style.width = `${image.offsetWidth}px`;
+    frame.style.height = `${image.offsetHeight}px`;
+
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+    });
+}());
