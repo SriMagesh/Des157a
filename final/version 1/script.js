aa9fe9037333128f760638b13d3f906469587b79
SriramMagesh <SriramMagesh@srmagesh.com>
2023-03-09 00:15:05 -0800

    Version 1

---
+++ final/version 1/script.js
@@ -0,0 +1,19 @@
+
+(function(){
+    'use strict';
+    console.log('reading js')
+
+    const scrollContainer = document.querySelector("main");
+
+    const frame = document.querySelector('.image-frame');
+    const image = frame.querySelector('.image1');
+
+    // Set the width and height of the frame to match the image size
+    frame.style.width = `${image.offsetWidth}px`;
+    frame.style.height = `${image.offsetHeight}px`;
+
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+    });
+}());
