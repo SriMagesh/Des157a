e86b6a57ed1469477641167bdb2814a31904c8c7
SriramMagesh <SriramMagesh@srmagesh.com>
2023-03-12 15:38:21 -0700

    comments

--- final/version 1/script.js
+++ final/version 1/script.js
@@ -7,7 +7,7 @@
 
     // updates left position of the image as user scrolls down
 
-    // this does not work... only works when I remove the horizontal scroll animation, but I need both to work
+    // this does not work...only works when I remove the horizontal scroll animation, but I need both to work
 
     // Intended effect: as user scrolls up the horizontal scroll will move left, alongside the bike, but will end where the frame ends
     window.addEventListener("scroll", (evt) => {
